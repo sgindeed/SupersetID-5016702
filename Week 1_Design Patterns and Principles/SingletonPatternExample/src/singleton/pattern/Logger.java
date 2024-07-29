9c71998de434f61874c25c786ab70259ae107119
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-07-29 20:45:58 +0530

    Add files via upload

---
+++ Week 1_Design Patterns and Principles/SingletonPatternExample/src/singleton/pattern/Logger.java
@@ -0,0 +1,22 @@
+package singleton.pattern;
+
+public class Logger {
+	
+	private static Logger instance;
+	
+    private Logger() {
+    	
+    }
+    
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+    
+    public void log(String message) {
+        System.out.println("Log message: " + message);
+    }
+
+}
