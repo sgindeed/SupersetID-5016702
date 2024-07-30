40b34a222b6695f185cc0fed7628031eefcd2a3d
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-07-30 18:18:04 +0530

    Add files via upload

---
+++ Week 1_Design Patterns and Principles/CommandPatternExample/src/command/pattern/CommandPatternExample.java
@@ -0,0 +1,18 @@
+package command.pattern;
+
+public class CommandPatternExample {
+    public static void main(String[] args) {
+        Light livingRoomLight = new Light();
+
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
---
+++ Week 1_Design Patterns and Principles/CommandPatternExample/src/command/pattern/RemoteControl.java
@@ -0,0 +1,13 @@
+package command.pattern;
+
+public class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
