7d96c7f95e0ad691cf19dbe50609c5942090dccd
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-08-06 11:21:37 +0530

    Add files via upload

---
+++ Week 2_Spring Core_Maven/LibraryManagement/src/main/java/com/library/org/MainApp.java
@@ -0,0 +1,17 @@
+package com.library.org;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    private static ApplicationContext context;
+
+	public static void main(String[] args) {
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        bookService.performService();
+    }
+}
---
+++ Week 2_Spring Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void performRepositoryOperation() {
+        System.out.println("BookRepository is performing operations...");
+    }
+}
